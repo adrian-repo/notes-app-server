0d3fc831bd6e2ce4e797e1d83b4bea146c45f3f4
“Adrian <adrian@mediasoft.one>
2018-11-04 15:27:43 +0100

    adding port

--- server.js
+++ server.js
@@ -114,4 +114,6 @@ app.post ('/deletenotes', (req,res) => {
       .catch(err => res.status(400).json("Something went wrong !"))
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+});
